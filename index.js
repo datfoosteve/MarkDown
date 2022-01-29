09005e9683b4cfe55f40b36a512582703e07dc87
Stephen Puthenpurackal <datfoosteve@gmail.com>
2022-01-29 03:48:55 -0600

    okay got it TO FREAKINW ORKKKASDSAJKDFNAKSFHJDHASD

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const inquirer = require("inquirer");
 const genReadme = require("./utils/generateMarkdown");
 const genReadmeTable = require("./utils/generateTableMarkdown");
 const fs = require("fs");
+const { nextTick, exit } = require("process");
+const { doesNotMatch } = require("assert");
 
 /* This code is used to ask the user if they want to generate a README.md file. */
 const askUserStart = {
   type: "list",
   name: "userChoice",
-  message: "Welcome. Would you want this program to generate you a README.md File?",
-  choices: [{
+  message:
+    "Welcome. Would you want this program to generate you a README.md File?",
+  choices: [
+    {
       name: "Yes",
     },
     {
@@ -17,145 +21,155 @@ const askUserStart = {
   ],
 };
 
-
 // an array of questions for user input
 const questions = [
   {
-    type: 'checkbox',
-    message: 'Preprocess inquiry Question: Please Select the sections that you want to Generate in your README.md .',
-    name: 'qConfirm',
+    type: "checkbox",
+    message:
+      "Preprocess inquiry Question: Please Select the sections that you want to Generate in your README.md .",
+    name: "qConfirm",
     choices: [
       {
-        name: 'titleConfirm',
-        message: "A title Section"
+        name: "titleConfirm",
+        message: "A title Section",
       },
       {
-        name: 'logoConfirm',
-        message: "A Program logo Section"
+        name: "logoConfirm",
+        message: "A Program logo Section",
       },
       {
-        name: 'descriptionConfirm',
-        message: "A Program Description Section"
+        name: "descriptionConfirm",
+        message: "A Program Description Section",
       },
       {
-        name: 'installConfirm',
-        message: "A Program install Section"
+        name: "installConfirm",
+        message: "A Program install Section",
       },
       {
-        name: 'usageConfirm',
-        message: "A Program Usage Section"
+        name: "usageConfirm",
+        message: "A Program Usage Section",
       },
       {
-        name: 'visualConfirm',
-        message: "A Program Visual Section"
+        name: "visualConfirm",
+        message: "A Program Visual Section",
       },
       {
-        name: 'linksConfirm',
-        message: "A Program Link Section"
+        name: "linksConfirm",
+        message: "A Program Link Section",
       },
       {
-        name: 'creditsConfirm',
-        message: "A Contribution/Credit Section"
+        name: "creditsConfirm",
+        message: "A Contribution/Credit Section",
       },
       {
-        name: 'usernameConfirm',
-        message: "A Question Section"
+        name: "usernameConfirm",
+        message: "A Question Section",
       },
       {
-        name: 'licenseConfirm',
-        message: "A Licensing Section"
+        name: "licenseConfirm",
+        message: "A Licensing Section",
       },
       {
-        name: 'statusConfirm',
-        message: "A Program Status Section"
+        name: "statusConfirm",
+        message: "A Program Status Section",
       },
       {
-        name: 'futureConfirm',
-        message: "A Program logo Section"
+        name: "futureConfirm",
+        message: "A Program logo Section",
       },
     ],
     validate(answer) {
       if (answer.length < 1) {
-        return 'You must atleast Generate one section in this README.md for this to be worthwhile...';
+        return "You must atleast Generate one section in this README.md for this to be worthwhile...";
       }
-  
+
       return true;
     },
   },
-  
-  
+
   {
     type: "input",
     name: "title",
     message: "What is the title of this Program Application?",
-    when: data => data.qConfirm.includes('titleConfirm') ? true : false
+    when: (data) => (data.qConfirm.includes("titleConfirm") ? true : false),
   },
   {
     type: "confirm",
     name: "logo",
     message: "Do you want img src boiler plate placed into the Logo Section??",
-    when: data => data.qConfirm.includes('logoConfirm') ? true : false
+    when: (data) => (data.qConfirm.includes("logoConfirm") ? true : false),
   },
   {
     type: "confirm",
     name: "visual",
     message: "Do you want img src boiler plate placed into the visual Section?",
-    when: data => data.qConfirm.includes('visualConfirm') ? true : false
+    when: (data) => (data.qConfirm.includes("visualConfirm") ? true : false),
+  },
+  new inquirer.Separator("NOTE: The next 3 questions will invoke a local text editor to intake Users input, but the editor might take a minute to instantiate, please be patient. "),
+  {
+    
+    type: "editor",
+    name: "description",
+    message:
+      "Please write a short description of this program.The length of the description must be ateast 2 lines.",
+      when: (data) =>
+      data.qConfirm.includes("descriptionConfirm") ? true : false,
+    validate(text) {
+      if (text.split("\n").length < 2) {
+        return "Must be at least 2 lines.";
+      }
+      return true;
+    },
   },
   {
-    type: 'editor',
-    name: "description",
-    message: 'Please write a short description of this program. The length of the description must be ateast 2 lines.',
-    when: data => data.qConfirm.includes('descriptionConfirm') ? true : false,
+    type: "editor",
+    name: "install",
+    message:
+      "Please write a short installation summary of the program in atleast 2 lines.",
+    when: (data) => (data.qConfirm.includes("installConfirm") ? true : false),
     validate(text) {
-      if (text.split('\n').length < 2) {
-        return 'Must be at least 2 lines.';
+      if (text.split("\n").length < 2) {
+        return "Must be at least 2 lines.";
       }
-   } },
+      return true;
+    },
+  },
   {
-    type: 'editor',
-    name: 'install',
-    message: 'Please write a short installation summary of the program in atleast 2 lines.',
-    when: data => data.qConfirm.includes('installConfirm') ? true : false,
+    type: "editor",
+    name: "usage",
+    message:
+      "Please write on how the usage is for this program in atleast 2 lines.",
+    when: (data) => (data.qConfirm.includes("usageConfirm") ? true : false),
     validate(text) {
-      if (text.split('\n').length < 2) {
-        return 'Must be at least 2 lines.';
+      if (text.split("\n").length < 2) {
+        return "Must be at least 2 lines.";
       }
-  }},
-  {
-    type: 'editor',
-    name: 'usage',
-    message: 'Please write on how the usage is for this program in atleast 2 lines.',
-    when: data => data.qConfirm.includes('usageConfirm') ? true : false,
-    validate(text) {
-      if (text.split('\n').length < 2) {
-        return 'Must be at least 2 lines.';
-      }
-  }
-},
+      return true;
+    },
+  }.then,
   {
     type: "input",
     name: "links",
     message: "Input your Deploy links if you have any.",
-    when: data => data.qConfirm.includes('linksConfirm') ? true : false,
+    when: (data) => (data.qConfirm.includes("linksConfirm") ? true : false),
   },
   {
     type: "input",
     name: "credits",
     message: "Input the creditors of this project.",
-    when: data => data.qConfirm.includes('creditConfirm') ? true : false,
+    when: (data) => (data.qConfirm.includes("creditConfirm") ? true : false),
   },
   {
     type: "input",
     name: "username",
     message: "Input your github username",
-    when: data => data.qConfirm.includes('usernameConfirm') ? true : false,
+    when: (data) => (data.qConfirm.includes("usernameConfirm") ? true : false),
   },
   {
     type: "list",
     name: "license",
     message: "What license would you like to include for this program?",
-    when: data => data.qConfirm.includes('licenseConfirm') ? true : false,
+    when: (data) => (data.qConfirm.includes("licenseConfirm") ? true : false),
     choices: [
       new inquirer.Separator(
         " =============================================== "
@@ -196,7 +210,7 @@ const questions = [
     type: "list",
     name: "status",
     message: "Status of the current project? ",
-    when: data => data.qConfirm.includes('statusConfirm') ? true : false,
+    when: (data) => (data.qConfirm.includes("statusConfirm") ? true : false),
     choices: [
       {
         name: "Finished and Finalized",
@@ -213,13 +227,13 @@ const questions = [
       {
         name: "Uncertain",
       },
-    ]
+    ],
   },
   {
     type: "input",
     name: "future",
     message: "Future Plans for this project?",
-    when: data => data.qConfirm.includes('futureConfirm') ? true : false,
+    when: (data) => (data.qConfirm.includes("futureConfirm") ? true : false),
   },
 ];
 
@@ -232,7 +246,7 @@ function writeToFile(fileName, data) {
 function init() {
   inquirer.prompt(questions).then((userAnswer) => {
     const readmeText = genReadme(userAnswer);
-    const readmetableText = genReadmeTable(userAnswer)
+    const readmetableText = genReadmeTable(userAnswer);
     console.clear();
     console.log("Answers recorded");
 
@@ -243,7 +257,9 @@ function init() {
     console.clear();
     console.log("Generation Complete");
 
-    console.log("Search your Distribution Folder in this file system for your Generated file");
+    console.log(
+      "Search your Distribution Folder in this file system for your Generated file"
+    );
     console.log("Goodbye");
   });
 }
